99b8dcf291aa97bc703276751d18bcbc9b74fd49
projecteinf <projecteinf@boscdelacoma.cat>
2025-06-10 04:17:39 -0600

    Habilitar notificacions - switch. Refactor - simplificació de codi

--- app/src/main/java/cat/mba/noactivity/features/settings/component/SettingsContent.kt
+++ app/src/main/java/cat/mba/noactivity/features/settings/component/SettingsContent.kt
@@ -1,11 +1,11 @@
 package cat.mba.noactivity.features.settings.component
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cat.mba.noactivity.R
 import cat.mba.noactivity.features.settings.component.library.LabelEnableNotifications
-import cat.mba.noactivity.features.settings.component.library.SwitchNotifications
 
 
 @Composable
@@ -28,13 +27,13 @@ fun SettingsContent(modifier: Modifier = Modifier) {
         var notificationsEnabled by remember { mutableStateOf(false) }
 
         LabelEnableNotifications(R.string.settings_enable_notifications)
-        SwitchNotifications(
-            isEnabled = notificationsEnabled,
-            onToggle =  {
-                    notificationsEnabled = it
-                    Log.d("Notifications enabled", notificationsEnabled.toString())
-            }
-        )
+
+        Column {
+            Switch(
+                checked = notificationsEnabled,
+                onCheckedChange = { notificationsEnabled = it }
+            )
+        }
 
        Spacer(modifier = Modifier.height(8.dp))
 
